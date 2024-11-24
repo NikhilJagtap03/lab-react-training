fcfbc16fc4086028cec0cc5356a96987956b7c65
Nikhil Vijay Jagtap <nikhilv.jagtap2003@gmail.com>
2024-11-24 12:37:27 +0530

    Done

---
+++ src/components/NumbersTable.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function NumbersTable({ limit }) {
+    
+let countList = []
+    for (let i = 1; i < limit; i++){
+    countList.push(i);
+    }
+  return (
+      <div style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(5, 0fr)",
+        gap: "0",
+      }}>
+          {countList.map((number)=>(
+              <div
+                  key={number}
+                  style={{
+                      background: number % 2 === 0 ? "red" : "white",
+                      padding: "20px",
+                      border: "1px solid black",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",            
+              }}>{number}</div>
+          ))}
+          
+    </div>
+  )
+}
